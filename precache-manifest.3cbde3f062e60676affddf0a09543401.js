1c270a1d853856b012c9e59b8f18aa80b3f9e97a
luizzitol <62354883+luizzitol@users.noreply.github.com>
2020-04-08 18:34:02 +0100

    Updates

--- precache-manifest.3cbde3f062e60676affddf0a09543401.js
+++ precache-manifest.3cbde3f062e60676affddf0a09543401.js
@@ -1,74 +1,74 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d6e931e5338de768a31d3041fdda237f",
-    "url": "/index.html"
+    "revision": "7e0a896f672da81af613e1a772ab5dab",
+    "url": "/PugGame/index.html"
   },
   {
     "revision": "80dad74e8761b7e5a779",
-    "url": "/static/css/main.53882406.chunk.css"
+    "url": "/PugGame/static/css/main.53882406.chunk.css"
   },
   {
     "revision": "368b797d0b6014360d92",
-    "url": "/static/js/2.8fe52030.chunk.js"
+    "url": "/PugGame/static/js/2.8fe52030.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.8fe52030.chunk.js.LICENSE.txt"
+    "url": "/PugGame/static/js/2.8fe52030.chunk.js.LICENSE.txt"
   },
   {
     "revision": "80dad74e8761b7e5a779",
-    "url": "/static/js/main.6f309d31.chunk.js"
+    "url": "/PugGame/static/js/main.6f309d31.chunk.js"
   },
   {
-    "revision": "f162b000793cd42b7d15",
-    "url": "/static/js/runtime-main.6023392c.js"
+    "revision": "d22d4bd930ff36884bc4",
+    "url": "/PugGame/static/js/runtime-main.7a5c021d.js"
   },
   {
     "revision": "4e6dd09b80af502bf6effc42e9ce7a2b",
-    "url": "/static/media/1.4e6dd09b.jpg"
+    "url": "/PugGame/static/media/1.4e6dd09b.jpg"
   },
   {
     "revision": "c2dd7afb2f0a34484f20109754da1e6c",
-    "url": "/static/media/2.c2dd7afb.jpg"
+    "url": "/PugGame/static/media/2.c2dd7afb.jpg"
   },
   {
     "revision": "0536e2e9ae9179b32aa10d733a1452fc",
-    "url": "/static/media/3.0536e2e9.jpg"
+    "url": "/PugGame/static/media/3.0536e2e9.jpg"
   },
   {
     "revision": "c20b9bf012154b1ebfdce0047d553ad9",
-    "url": "/static/media/4.c20b9bf0.jpg"
+    "url": "/PugGame/static/media/4.c20b9bf0.jpg"
   },
   {
     "revision": "4167f4282907c200914a3b8dfc455b39",
-    "url": "/static/media/5.4167f428.jpg"
+    "url": "/PugGame/static/media/5.4167f428.jpg"
   },
   {
     "revision": "4cb2f926f42c35f3ce3fc60fb1768244",
-    "url": "/static/media/6.4cb2f926.jpg"
+    "url": "/PugGame/static/media/6.4cb2f926.jpg"
   },
   {
     "revision": "e610cbbe9147dedd486ee377b895e499",
-    "url": "/static/media/7.e610cbbe.jpg"
+    "url": "/PugGame/static/media/7.e610cbbe.jpg"
   },
   {
     "revision": "a81a3e11ef7f3a9f69bfdc1eb44f50e9",
-    "url": "/static/media/8.a81a3e11.jpg"
+    "url": "/PugGame/static/media/8.a81a3e11.jpg"
   },
   {
     "revision": "0a84987625292eaa2acdd443f89fd000",
-    "url": "/static/media/9.0a849876.jpg"
+    "url": "/PugGame/static/media/9.0a849876.jpg"
   },
   {
     "revision": "d405282bff805e75583c908c455963ee",
-    "url": "/static/media/bark.d405282b.mp3"
+    "url": "/PugGame/static/media/bark.d405282b.mp3"
   },
   {
     "revision": "b535b5f03a79f7503957c7af7391ca25",
-    "url": "/static/media/bg.b535b5f0.jpg"
+    "url": "/PugGame/static/media/bg.b535b5f0.jpg"
   },
   {
     "revision": "5caa864283ebffe81d7b01b5c9d47b64",
-    "url": "/static/media/wrong.5caa8642.mp3"
+    "url": "/PugGame/static/media/wrong.5caa8642.mp3"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e8f4ee5a0acdd642dfaef21f50fc496.js"
+  "/PugGame/precache-manifest.3cbde3f062e60676affddf0a09543401.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PugGame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
